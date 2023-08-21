ae6e9d43772be7e6834f9d729f186135924c2bfa
tortudev <cesosa8@gmail.com>
2023-08-21 17:35:20 -0400

    workEnd

---
+++ main.js
@@ -0,0 +1,103 @@
+const mouseOne = document.querySelector('.mouseOne');
+const mouseTwo = document.querySelector('.mouseTwo');
+const mouseThree = document.querySelector('.mouseThree');
+const mouseFour = document.querySelector('.mouseFour');
+const mouseFive = document.querySelector('.mouseFive');
+const mouseSix = document.querySelector('.mouseSix');
+const mouseSeven = document.querySelector('.mouseSeven');
+const mouseEight = document.querySelector('.mouseEight');
+const mouseNine = document.querySelector('.mouseNine');
+const mouseTen = document.querySelector('.mouseTen');
+const mouseEleven = document.querySelector('.mouseEleven');
+const mouseTwelve = document.querySelector('.mouseTwelve');
+const mouseThirteen = document.querySelector('.mouseThirteen');
+const mouseFourteen = document.querySelector('.mouseFourteen');
+const mouseFifteen = document.querySelector('.mouseFifteen');
+const mouseSixteen = document.querySelector('.mouseSixteen');
+const mouseSeventeen = document.querySelector('.mouseSeventeen');
+const mouseEighteen = document.querySelector('.mouseEighteen');
+const mouseNineteen = document.querySelector('.mouseNineteen');
+
+mouseOne.addEventListener('mouseover', () => {
+    mouseOne.style.cursor = "none";
+});
+
+mouseTwo.addEventListener('mouseover', () => {
+    mouseTwo.style.cursor = "default";
+});
+
+mouseThree.addEventListener('mouseover', () => {
+    mouseThree.style.cursor = "crosshair";
+});
+
+mouseFour.addEventListener('mouseover', () => {
+    mouseFour.style.cursor = "n-resize";
+});
+
+mouseFive.addEventListener('mouseover', () => {
+    mouseFive.style.cursor = "e-resize";
+});
+
+mouseSix.addEventListener('mouseover', () => {
+    mouseSix.style.cursor = "zoom-in";
+});
+
+mouseSeven.addEventListener('mouseover', () => {
+    mouseSeven.style.cursor = "zoom-out";
+});
+
+mouseEight.addEventListener('mouseover', () => {
+    mouseEight.style.cursor = "alias";
+});
+
+mouseNine.addEventListener('mouseover', () => {
+    mouseNine.style.cursor = "all-scroll";
+});
+
+mouseTen.addEventListener('mouseover', () => {
+    mouseTen.style.cursor = "cell";
+});
+
+mouseEleven.addEventListener('mouseover', () => {
+    mouseEleven.style.cursor = "copy";
+});
+
+mouseTwelve.addEventListener('mouseover', () => {
+    mouseTwelve.style.cursor = "grab";
+});
+
+mouseThirteen.addEventListener('mouseover', () => {
+    mouseThirteen.style.cursor = "help";
+});
+
+mouseFourteen.addEventListener('mouseover', () => {
+    mouseFourteen.style.cursor = "not-allowed";
+});
+
+mouseFifteen.addEventListener('mouseover', () => {
+    mouseFifteen.style.cursor = "col-resize";
+});
+
+mouseSixteen.addEventListener('mouseover', () => {
+    mouseSixteen.style.cursor = "pointer";
+});
+
+mouseSeventeen.addEventListener('mouseover', () => {
+    mouseSeventeen.style.cursor = "progress";
+});
+
+mouseEighteen.addEventListener('mouseover', () => {
+    mouseEighteen.style.cursor = "text";
+});
+
+mouseNineteen.addEventListener('mouseover', () => {
+    mouseNineteen.style.cursor = "wait";
+});
+
+
+
+
+
+
+
+
